067a58ba6a38317116fc6466d1049d09ee5b3837
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-08-17 22:40:49 -0400

    adding css styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import "./Header.css";
 
 const Header = () => (
   <header className = "jumbotron">
-    <h1>Star Trek Click Game</h1>
-    <h3>Try to avoid clicking the same image more than once after they shuffle</h3>
+    <h1>Choose Your Pokemon</h1>
+    <h3>Try to catch only one pokemon ONCE!</h3>
   </header>
 );
 
