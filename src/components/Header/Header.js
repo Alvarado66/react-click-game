0af1b0a2db30d4f19e2a00e775904ec3b4c02b44
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-08-18 21:53:18 -0400

    redo of some pokemon pictures

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 const Header = () => (
   <header className = "jumbotron">
     <h1>Choose Your Pokemon</h1>
-    <h3>Try to catch only one pokemon ONCE!</h3>
+    <h3>Gotta Catch'em All!</h3>
   </header>
 );
 
